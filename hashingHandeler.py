6c43046c866d9164d573d25449c7921b1362ef6f
TheKettleEngine <95769934+KettleEngine@users.noreply.github.com>
2023-11-24 02:17:01 +0000

    finished the dicing function and added in salting
    finally finished the dicing function and got it all integrated with the hashing function so that hashing is super easy to do, all you need to do is pass the data you want to hash, salt and dice to the hash function and it will automatically do the rest and it even has an optional parameter for a custom salting string if required later down the line.

--- hashingHandeler.py
+++ hashingHandeler.py
@@ -6,7 +6,14 @@ class Hash():
         pass
     
     #to hash data like passwords to protect them incase of a data leak
-    def hash(self, data):
+    def hash(self, data, salt="Default"):
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(data.encode())
+        #return sha256_hash.hexdigest()
+        
+        return self.salt(sha256_hash.hexdigest(), salt)
+        
+    def hashSalt(self, data):
         sha256_hash = hashlib.sha256()
         sha256_hash.update(data.encode())
         return sha256_hash.hexdigest()
@@ -14,8 +21,10 @@ class Hash():
     #data is what will be salted (so stuff like passwords) and salt is what will be used to salt it
     def salt(self, data, salt):
         #hash the salt so that it looks all the same for the next step
-        saltedData = data+hash(salt)
-        return saltedData
+        salt = self.hashSalt(salt)
+        
+        saltedData = data+salt
+        return self.dicing(saltedData)
     
     #this is a concept that i have been engineering in my head for a while and bassically it takes the hashed string
     #and divides it into many small little peices like how a cheff would dice an onion. then it takes all the small
@@ -24,18 +33,18 @@ class Hash():
     def dicing(self, data):
         #first get the length of the hashed string, this allos me to devide the string up roughly evenly
         #all of them should be 64 characters long but just incase it is best for it to be dynamic
+        backupData = data
         length = len(data)
-        print(length)
         
         #next part is to just try and devide the length as much as possible to get an amount of groups ofcharacters
         #this will be done based on the length of the string so for a 64 length there may be 16 groups of 4 but 32 may have 8 groups of 4
         dices = length // 4 #4 is how big i am going to make each diced segment
-        print(dices)
         
         #note to self: i can't wait to get this working only to find out a system like this is already been made and better than my one
         
         #next part is to split up each part of the string into groups of 4
         dicesList = []
+        copylist = []
         
         #to start with we need a for loop to dice each segment
         for i in range(0, dices):
@@ -47,10 +56,57 @@ class Hash():
             data = data[4:]
         
         #next step is to shuffle the diced segments around in a pattern that is consistant
-        print(dicesList)
         #the best way i have thought of to do this is to convert each character in each segment into their 
-        #dinary representation. then sort the array, thenconvert it back into a string and concatinate the strings together
+        #ASCII representation. then sort the array, then convert it back into a string and concatinate the strings together
         
+        asciiList = []
         
+        for x in range(0, dices):
+            #this is a for loop to go through each character in each dice segment, it will dynamically adapt
+            #to the length of the string incase there is an odd amount in the hash
             
+            temp = []
+            temp2 = []
+
+            temp.append(x)
+            temp2.append(x)
+            sumTemp = 0
+            
+            for z in range(0, len(dicesList[x])):
+                
+                sumTemp = sumTemp + ord(dicesList[x][z])
+            
+            temp.append(sumTemp)
+            
+            #adds the 4 letter string to a coppy of the array with the ndex numbers so it can be easily put back together later
+            temp2.append(dicesList[x])
+                 
+            copylist.append(temp2)     
+            asciiList.append(temp)
+            
+        #now that we have the cottect lists in a useful format we can sort them 
+        
+        #sort the array
+        asciiList = self.sortArrays(asciiList)
+        
+        
+        dicedHash = ""
+        #next is to just concatinate the data from the copylist in the order that the ascii array is sorted into
+        for i in range(0, len(asciiList)):
+            dicedHash = dicedHash + copylist[asciiList[i][0]][1]
+            
+        return dicedHash
+            
+                
+    
+    def sortArrays(self, list):
+        for passnum in range(len(list)-1,0,-1):
+            for i in range(passnum):
+                if list[i][1]>list[i+1][1]:
+                    temp = list[i]
+                    list[i] = list[i+1]
+                    list[i+1] = temp
+        
+        return list
+
             
--- test.py
+++ test.py
@@ -15,5 +15,24 @@ pw = "test"
 #print(meth.checkEmail("cs"))
 
 #print(meth.login(email, pw))
+#print(ord("1"))
 
-hash.dicing(hash.hash(pw))
+
+
+def checkHashes(hash1, hash2):
+    if hash1 == hash2:
+        return True
+    else:
+        return False
+
+hashed1 = "470621b10a08d015a5041c43d081822c6c152d18a955d548b9827d65884c9f86b0a02b0b11cbb0f0b22e4f1bc55ad15d7a2df53a38ac9a2feaa0a3bf8fcafe6e"
+hashed2 = "5117470621b1a5041c432d189c13953ac547a95584c6d548b982b497b0a011cb1c6ab22eaa835c4b7a2df53a38ac4f4c59ae5eb8fb57b7e88fcafe6ef7eeeecb"
+
+testString1 = "test"
+testString2 = "test"
+    
+print(checkHashes(hash.hash(testString1), hash.hash(testString2)))
+
+print(hash.hash(testString1))
+
+print(hash.hash(testString2))
